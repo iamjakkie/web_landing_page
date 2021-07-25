f933b92043fd84ebe14e6f3359119d029b519ef8
Jakkie <jakubkielbasiewicz@gmail.com>
2021-07-25 23:59:15 +0200

    Initial solution

--- js/app.js
+++ js/app.js
@@ -13,11 +13,14 @@
  * 
 */
 
+
+
 /**
  * Define Global Variables
  * 
 */
-
+const nav = document.getElementById('navbar__list');
+const section = document.querySelectorAll('section');
 
 /**
  * End Global Variables
@@ -25,6 +28,21 @@
  * 
 */
 
+const offset = (section) => {
+    return Math.floor(section.getBoundingClientRect().top);
+};
+
+const removeActive = (section) => {
+    section.classList.remove('your-active-class');
+    section.style.cssText = "background-color: linear-gradient(0deg, rgba(255,255,255,.1) 0%, rgba(255,255,255,.2) 100%)";
+};
+
+const addActive = (conditional, section) => {
+    if(conditional){
+        section.classList.add('your-active-class');
+        section.style.cssText = "background-color: yellow;";
+    };
+};
 
 
 /**
@@ -34,13 +52,43 @@
 */
 
 // build the nav
+const navBuilder = () => {
+    let navHTML = '';
+    section.forEach(section => {
 
+        const sectionID = section.id;
+        const sectionDataNav = section.dataset.nav;
+
+        navHTML += `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`;
+    });
+    nav.innerHTML = navHTML;
+};
 
 // Add class 'active' to section when near top of viewport
+const sectionActivation = () => {
+    section.forEach(section => {
+        const elementOffset = offset(section);
 
+        inviewport = () => elementOffset < 150 && elementOffset >= -150;
+
+        removeActive(section);
+        addActive(inviewport(),section);
+    });
+};
 
 // Scroll to anchor ID using scrollTO event
+const scrolling = () => {
 
+    const links = document.querySelectorAll('.navbar__menu a');
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            for(i = 0 ; i<section ; i++){
+                section[i].addEventListener("click",sectioncroll(link));
+            }
+        });
+    });
+
+};
 
 /**
  * End Main Functions
@@ -48,10 +96,14 @@
  * 
 */
 
+
 // Build menu 
-
+navBuilder();
 // Scroll to section on link click
-
+window.addEventListener('scroll' ,sectionActivation);
+scrolling();
 // Set sections as active
+
+
 
 

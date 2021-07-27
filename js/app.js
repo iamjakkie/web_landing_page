3d31b18d3d7339d9e68eb58ad38dd43cf72175da
Jakkie <jakubkielbasiewicz@gmail.com>
2021-07-28 00:18:21 +0200

    revert to blank js part

--- js/app.js
+++ js/app.js
@@ -13,14 +13,11 @@
  * 
 */
 
-
-
 /**
  * Define Global Variables
  * 
 */
-const nav = document.getElementById('navbar__list');
-const section = document.querySelectorAll('section');
+
 
 /**
  * End Global Variables
@@ -28,21 +25,6 @@ const section = document.querySelectorAll('section');
  * 
 */
 
-const offset = (section) => {
-    return Math.floor(section.getBoundingClientRect().top);
-};
-
-const removeActive = (section) => {
-    section.classList.remove('your-active-class');
-    section.style.cssText = "background-color: linear-gradient(0deg, rgba(255,255,255,.1) 0%, rgba(255,255,255,.2) 100%)";
-};
-
-const addActive = (conditional, section) => {
-    if(conditional){
-        section.classList.add('your-active-class');
-        section.style.cssText = "background-color: yellow;";
-    };
-};
 
 
 /**
@@ -52,43 +34,13 @@ const addActive = (conditional, section) => {
 */
 
 // build the nav
-const navBuilder = () => {
-    let navHTML = '';
-    section.forEach(section => {
 
-        const sectionID = section.id;
-        const sectionDataNav = section.dataset.nav;
-
-        navHTML += `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`;
-    });
-    nav.innerHTML = navHTML;
-};
 
 // Add class 'active' to section when near top of viewport
-const sectionActivation = () => {
-    section.forEach(section => {
-        const elementOffset = offset(section);
 
-        inviewport = () => elementOffset < 150 && elementOffset >= -150;
-
-        removeActive(section);
-        addActive(inviewport(),section);
-    });
-};
 
 // Scroll to anchor ID using scrollTO event
-const scrolling = () => {
 
-    const links = document.querySelectorAll('.navbar__menu a');
-    links.forEach(link => {
-        link.addEventListener('click', () => {
-            for(i = 0 ; i<section ; i++){
-                section[i].addEventListener("click",sectioncroll(link));
-            }
-        });
-    });
-
-};
 
 /**
  * End Main Functions
@@ -96,14 +48,8 @@ const scrolling = () => {
  * 
 */
 
-
 // Build menu 
-navBuilder();
+
 // Scroll to section on link click
-window.addEventListener('scroll' ,sectionActivation);
-scrolling();
+
 // Set sections as active
-
-
-
-
